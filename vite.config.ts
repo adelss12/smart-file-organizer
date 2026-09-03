866bf4391131dbf32cd68bf158be8992c2a177e1
adelss12 <iadelss12@gmail.com>
2026-09-03 12:38:50 +0300

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/smart-file-organizer/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
